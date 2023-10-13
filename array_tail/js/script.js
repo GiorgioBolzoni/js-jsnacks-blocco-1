0b519e5ea7083e66587c67878ff97de2579ec7f1
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-13 16:22:13 +0200

    creato array che contiente un numero casuale a 1 a 100

---
+++ array_tail/js/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector('button')
+const inputEl = document.querySelector('data')
+const resultEl = document.querySelector('.alert')
+// creo array che contiene numeri casuali da 0 a 100
+const tail = [getRndInteger(1, 100)];
+console.log(tail)
