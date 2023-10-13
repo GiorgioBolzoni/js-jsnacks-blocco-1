b2559132d1f32fbedccad51fcdf5dedaba7ed6bc
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-13 15:53:11 +0200

    creata struttura di base js con array e aggiunto "pesca" all'array

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,6 +1,21 @@
 const btn = document.querySelector('button')
 const inputEl = document.querySelector('data')
 const resultEl = document.querySelector('.alert')
+// creo l'array con la frutta del frigorifero
+const frutta = [
+    'banana',
+    'mela',
+    'pera',
+    'ciliegia',
+    'arancia',
+    'mandarino',
+    'cocomero',
+    'limone',
+    'fragola',
+];
+// console.log(frutta);
+frutta.push('pesca');
+console.log(frutta);
 
 btn.addEventListener('click', function(){
     // inputEl.value
