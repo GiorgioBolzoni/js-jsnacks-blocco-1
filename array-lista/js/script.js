f586e88566c459e8e57e3da8b053514814346e54
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-13 15:20:40 +0200

    scaffolding iniziale

---
+++ array-lista/js/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button')
+const inputEl = document.querySelector('data')
+const resultEl = document.querySelector('.alert')
+
+btn.addEventListener('click', function(){
+    // inputEl.value
+})
